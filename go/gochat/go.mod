2875e4129e914f02f0dbcd61908d463600adf84d
houming818 <houming818@qq.com>
2024-03-08 09:32:31 +0000

    none

---
+++ go/gochat/go.mod
@@ -0,0 +1,5 @@
+module grepcode.cn/gochat
+
+go 1.21.6
+
+require github.com/streadway/amqp v1.1.0 // indirect
---
+++ go/gochat/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func setupConsumer(ch *amqp.Channel, exchangeName string) {
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.QueueBind(
+		q.Name,       // queue name
+		"",           // routing key
+		exchangeName, // exchange
+		false,
+		nil)
+	failOnError(err, "Failed to bind a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+}
+
+func sendMessage(ch *amqp.Channel, exchangeName, message string) {
+	err := ch.Publish(
+		exchangeName, // exchange
+		"",           // routing key
+		false,        // mandatory
+		false,        // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
+	failOnError(err, "Failed to publish a message")
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	exchangeName := "chat_ex"
+	err = ch.ExchangeDeclare(
+		exchangeName, // name
+		"fanout",     // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	setupConsumer(ch, exchangeName)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter messages. To exit press CTRL+C.")
+	for {
+		fmt.Print("> ")
+		msg, _ := reader.ReadString('\n')
+		sendMessage(ch, exchangeName, msg)
+	}
+}
